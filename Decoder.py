157f022d63660c364d9fef67a83aa6faf2f7d478
xccount <47496853+xccount@users.noreply.github.com>
2024-02-29 18:14:57 +0530

    Initial commit

---
+++ Decoder.py
@@ -0,0 +1,74 @@
+from tabulate import tabulate
+
+print("LACP Actor Status Decoder", '\n')
+
+while True:
+        print('')
+        inpt = input("Type Port-State Code: ")
+        if inpt == 'help':
+                print('yes')
+                continue
+        
+        if inpt:
+                inpt_int = int(inpt, 16)
+                inpt_bin = bin(inpt_int)
+                inpt_bin = inpt_bin[2:].zfill(8)[::-1]
+
+                #string to digit
+                bin_arry = [int(x) for x in str(inpt_bin)]
+
+                #variables
+                if bin_arry[0] == 1:
+                        LACP_Activity = "Active"
+                else:
+                    LACP_Activity = "Passive"
+
+                if bin_arry[1] == 1:
+                        LACP_Timeout = "Short Timeout"
+                else:
+                        LACP_Timeout = "Long Timeout"
+                    
+                if bin_arry[2] == 1:
+                        Aggregation = "Yes"
+                else:
+                    Aggregation = "No (individual link)"
+                    
+                if bin_arry[3] == 1:
+                        Synchronization = "In sync"
+                else:
+                    Synchronization = "NOT in sync"
+
+                if bin_arry[4] == 1:
+                        Collecting = "Mux is Collecting"
+                else:
+                    Collecting = "Mux is NOT Collecting"
+
+                if bin_arry[5] == 1:
+                        Distributing = "Mux is Distributing"
+                else:
+                    Distributing = "Mux is NOT Distributing"
+
+                if bin_arry[6] == 1:
+                        Defaulted = "Using Default PDU"
+                else:
+                    Defaulted = "via LACP PDU"
+
+                if bin_arry[7] == 1:
+                        Expired = "Yes"
+                else:
+                    Expired = "No"
+
+
+                print ('\n',str.upper(inpt),"=",inpt_bin, '(Hexadecimal converted to Binary)','\n')
+                print (tabulate([[bin_arry[0], "LACP Activity", LACP_Activity],
+                                [bin_arry[1], "LACP Timeout", LACP_Timeout],
+                                [bin_arry[2], "Aggregation", Aggregation],
+                                [bin_arry[3], "Synchronization", Synchronization],
+                                [bin_arry[4], "Collecting", Collecting],
+                                [bin_arry[5], "Distributing", Distributing],
+                                [bin_arry[6], "Defaulted", Defaulted],
+                                [bin_arry[7], "PDU Expired", Expired]],
+                                headers=['Bit','FLAGS', 'STATE']))
+                continue
+        else:
+                print("Type 'help' for the details.", '\n')
