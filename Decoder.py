87897794fbcd47ff2c1d41f49a0edbfab103b379
xccount <47496853+xccount@users.noreply.github.com>
2024-03-02 21:18:07 +0530

    Completed

--- Decoder.py
+++ Decoder.py
@@ -10,23 +10,23 @@ while True:
         inpt = input("Type Port-State Code: ")
         if inpt == 'help':
                 clear()
-                print ('\n',tabulate([["LACP Activity", '0 - adfasdfasdf'],
-                                 ['', '1 - adfadfasdf'],
-                                ["LACP Timeout", '0'],
-                                ['', '1'],
-                                ["Aggregation", '0'],
-                                ['', '1'],
-                                ["Synchronization", '0'],
-                                ['', '1'],
-                                ["Collecting", '0'],
-                                ['', '1'],
-                                ["Distributing", '0'],
-                                ['', '1'],
-                                ["Defaulted", '0'],
-                                ['', '1'],
-                                ["PDU Expired", '0'],
-                                ['', '1']],
-                                headers=['FLAGS', 'STATES'], tablefmt="simple", colalign=("left",)))
+                print ('\n',tabulate([["LACP Activity", '0 - LACP mode Passive.'],
+                                 ['', '1 - LACP mode Active.'],
+                                ["LACP Timeout", '0 - LACP timeout is Short.'],
+                                ['', '1 - LACP timeout is Long.'],
+                                ["Aggregation", '0 - Link is NOT ready to be aggregated.'],
+                                ['', '1 - Link is ready to be aggregated.'],
+                                ["Synchronization", '0 - LACP frames are NOT in sync with partner.'],
+                                ['', '1 - LACP frames are in sync with partner.'],
+                                ["Collecting", '0 - Mux is Rejecting traffic received on this port.'],
+                                ['', '1 - Accepting traffic received on this port.'],
+                                ["Distributing", '0 - Mux is NOT sending traffic from this port.'],
+                                ['', '1 - Mux is NOT sending traffic from this port.'],
+                                ["Defaulted", '0 - Mux received parameters via LACP PDU.'],
+                                ['', '1 - Mux is using admin defined parameters.'],
+                                ["PDU Expired", '0 - is not in expired state.'],
+                                ['', '1 - is in expired state.']],
+                                headers=['FLAGS', 'STATES'], tablefmt="simple", colalign=("left",)),'\n\n*Mux - Multiplexer refers to the logical unit aggregates physical links into single logical unit.\n')
                 continue
         
         if inpt:
@@ -79,7 +79,7 @@ while True:
                     Expired = "No"
 
                 clear()
-                print ('\n',str.upper(inpt),"=",inpt_bin, '(Hexadecimal converted to Binary)','\n')
+                print ('\n',str.upper(inpt),"=",inpt_bin, '(Hexadecimal converted to Binary and flipped)','\n')
                 print (tabulate([[bin_arry[0], "LACP Activity", LACP_Activity],
                                 [bin_arry[1], "LACP Timeout", LACP_Timeout],
                                 [bin_arry[2], "Aggregation", Aggregation],
@@ -92,6 +92,6 @@ while True:
                 continue
         else:
                 clear()
-                print("Type mentioed port-state hex code to decode.")
-                print("Type 'help' for the details.")
-                print("Press'ctrl+c' to close the application.", '\n')
+                print("> Type LACP Port-State hex code to decode.")
+                print("> Type 'help' for the details.")
+                print("> Press'ctrl+c' to close the application.", '\n')
